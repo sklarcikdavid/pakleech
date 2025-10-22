8660c2adfbc465b573ddc8313b8914a9c4944edf
sklarcikdavid <sklarcik.david@gmail.com>
2025-10-22 12:43:05 +0200

    návrh úvodní obrazovky

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,15 @@
 const App = () => {
   return (
-    <h1>PAKLEECH - Klíčník tvého zařízení</h1>
+    <nav>
+        <div class="obrazekVmainu">
+          <img src="https://danny-p.s18.cdn-upgates.com/_cache/c/a/ca5df5e36eed3dca68974c4917da26ef-k2c-bm-01.jpg" alt="obrazecek" />
+        </div>
+        <div class="velkyNapisNaZacatku">
+          <h1>PAKLEECH</h1>
+          <h2>klíčník tvým zařízením</h2>
+        </div>
+    </nav>
   )
 }
+
+export default App

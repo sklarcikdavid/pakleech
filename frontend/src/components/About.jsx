0987d8296b958ea6d1dd28054e383ca2560cf0c0
sklarcikdavid <sklarcik.david@gmail.com>
2025-10-30 13:55:57 +0100

    About.jsx navrh
    pořád si nejsem uplne jisty jestli to chci udelat takhle, ale z duvodu nedostatku napadu to tak zatim necham. Možná až mě něco napadne, tak to vyměním, ale zatím mě nenapadá nic.

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,8 +1,22 @@
 const About = () => {
     return (
-        <div>
-            <p className="text-6xl font-black text-primary">About Us</p>
-        </div>
+        <section className="flex justify-between">
+            <div className="w-[300px] flex justify-center bg-amber-500 h-[20rem] ml-12 mb-12 mt-O rounded-2xl">
+                <p className="text-3xl">
+                    Co děláme?
+                </p>
+            </div>
+            <div className="w-[300px] flex justify-center bg-amber-500 h-[20rem] ml-12 mb-12 mt-O rounded-2xl">
+                <p className="text-3xl">
+                    Proč děláme?
+                </p>
+            </div>
+            <div className="w-[300px] flex justify-center bg-amber-500 h-[20rem] ml-12 mb-12 mt-O mr-12 rounded-2xl">
+                <p className="text-3xl">
+                    Jak děláme?
+                </p>
+            </div>
+        </section>
     )
 }
 export default About

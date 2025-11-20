2b3b3c7eeb2fb28c640172d5f5acc54ca9ed6fdf
sklarcikdavid <sklarcik.david@gmail.com>
2025-11-20 20:21:10 +0100

    Hodně změn
    Už pomalu dělám na velkém i malém logu, stáhnul jsem framer-motion pro efekty, které mám poté v plánu přidávat. Od plánu s 3D modely jsem nějak odešel, ale ve složkách je ještě nechávám. Stáhnul jsem SF-Pro font, tím že dělám na Mac zažízení, tak by ostatní neměli stejný font jako já, tak jsem ho stáhnul ať je všude stejný. A nastavil jsem ještě window.title změny když je okno neaktivní, tak to se mi líbí :D Catch-phrase "Don't forget your keys!" (Nezapomeň si klíče!) mi přijde vtipná.
    
    AboutUs bych chtěl udělat jako parallax, proto jsem stáhnul framer-motion, budu si muset vymyslet pár věcí, co tam napsat, ale vidím to tak, že do toho dám to, co je napsané v README.md

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
                     <img src="https://static.sigosigns.com/media/catalog/product/cache/3c7b6684964f1372900e9fb79dcea243/_/s/_s_i_si-70709-12-m.jpg" className="h-32 mr-4" alt="pakleech" />
                 </div>
                 <div class="HeroHeader" className="flex flex-col">
-                    <h1 className="text-text text-8xl font-bold mb-0">pakleech.</h1>
+                    <h1 className="text-text text-8xl font-bold mb-0">pakleech</h1>
                     <p className="text-text text-2xl font-light mt-0 ml-72">klíčník tvým zařízením</p>
                 </div>
             </div>
